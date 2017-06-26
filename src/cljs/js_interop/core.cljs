17ab5b8b9321c3ef74075a0b47dc2b3ab378be6c
John Stevenson <john@jr0cket.co.uk>
2017-06-26 16:40:21 +0100

    Added more examples of Javascript interop

--- src/cljs/js_interop/core.cljs
+++ src/cljs/js_interop/core.cljs
@@ -22,3 +22,33 @@
 
 ;; (def text js/globalName)
 ;; JS output: namespace.text = globalName;
+
+(def s "The octopus concoted colorized octane.")
+
+(type s)
+;;=> #object[String "function String() {
+;;       [native code]
+;;   }"]
+
+(.toUpperCase s)
+
+;; Property access
+;; use .-propertyName to get the value of an objects property with that name. If you do not use the .- then ClojureScript will assume the property is bound to a function and call it.
+(.-length s)
+
+;; rather than write this out in full
+(.toUpperCase (.toString (.charCodeAt s 7) 16))
+
+;; use the double dot notation
+(.. s (charCodeAt 7) (toString 16) toUpperCase)
+
+(set! js/newVar 3)
+js/newVar
+
+(set! (.. js/document (getElementById "app") -innerHTML) "Hello, world!")
+(set! (.-innerHTML (.getElementById js/document "app")) "Hello, world!!!")
+
+(def arr (array 1 2 3))
+(def obj (js-obj "x" 1 "y" 2))
+
+(aget arr 1)
